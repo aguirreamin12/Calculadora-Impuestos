ef500efd15bbd4985c0cd4cafb5011be7a376696
Santino Aguirre Amin <santinoaguirre2004@gmail.com>
2022-08-04 17:57:24 -0300

    Actualizacion dolar

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ let resultado = document.getElementById("result");
 let input1 = document.getElementById("name");
 
 input1.addEventListener('input', () => {
-    resultado.innerHTML = (input1.value * 235.6);
+    resultado.innerHTML = (input1.value * 244.42);
 })
